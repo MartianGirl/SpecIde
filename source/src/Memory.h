9b71e6de2d6d7c150b381f64c10eeea6dfc059ea
Marta Sevillano <sevillano.marta@gmail.com>
2016-12-11 02:26:25 +0100

    Comments on memory pinout.

--- source/src/Memory.h
+++ source/src/Memory.h
@@ -7,6 +7,19 @@
  *
  * Memory bank.
  *
+ * This is intended to be a TMS4116-like memory. The pinout is:
+ * 1. VBB (-5V)                     16. VSS (GND)
+ * 2. D                             15. CAS# (Column Address Strobe)
+ * 3. W#                            14. Q
+ * 4. RAS# (Row Address Strobe)     13. A6
+ * 5. A0                            12. A3
+ * 6. A2                            11. A4
+ * 7. A1                            10. A5
+ * 8. VDD (12V)                      9. VCC (5V)
+ * 
+ * The point here is, the memories are asynchronous. The addresses are
+ * latched by the CAS#/RAS# signals.
+ *
  */
 
 using namespace std;
