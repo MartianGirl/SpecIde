beeb2aa39a2eb8c102fe6a4faf443ae37518f000
MartianGirl <sevillano.marta@gmail.com>
2016-12-31 15:48:50 +0100

    Register tables for DD & FD prefixed opcodes.

--- source/src/Z80RegisterSet.cc
+++ source/src/Z80RegisterSet.cc
@@ -6,15 +6,35 @@ Z80RegisterSet::Z80RegisterSet() :
     bc(&bc_pair[registerSet]),
     de(&de_pair[registerSet]),
     hl(&hl_pair[registerSet]),
-    r8{{&bc_pair[0].h, &bc_pair[0].l, &de_pair[0].h, &de_pair[0].l,
-        &hl_pair[0].h, &hl_pair[0].l, nullptr, &af_pair[0].h},
-    {&bc_pair[1].h, &bc_pair[1].l, &de_pair[1].h, &de_pair[1].l,
-        &hl_pair[1].h, &hl_pair[1].l, nullptr, &af_pair[1].h}},
-    rp{{&bc_pair[0].w, &de_pair[0].w, &hl_pair[0].w, &sp.w},
-        {&bc_pair[1].w, &de_pair[1].w, &hl_pair[1].w, &sp.w}},
-    rp2{{&bc_pair[0].w, &de_pair[0].w, &hl_pair[0].w, &af_pair[0].w},
-        {&bc_pair[1].w, &de_pair[1].w, &hl_pair[1].w, &af_pair[1].w}},
+    r8{
+        {&bc_pair[0].h, &bc_pair[0].l, &de_pair[0].h, &de_pair[0].l,
+            &hl_pair[0].h, &hl_pair[0].l, nullptr, &af_pair[0].h},
+        {&bc_pair[1].h, &bc_pair[1].l, &de_pair[1].h, &de_pair[1].l,
+            &hl_pair[1].h, &hl_pair[1].l, nullptr, &af_pair[1].h}
+    },
+    rx8{
+        {&bc_pair[0].h, &bc_pair[0].l, &de_pair[0].h, &de_pair[0].l,
+            &ix.h, &ix.l, nullptr, &af_pair[0].h},
+        {&bc_pair[1].h, &bc_pair[1].l, &de_pair[1].h, &de_pair[1].l,
+            &ix.h, &ix.l, nullptr, &af_pair[1].h}
+    },
+    ry8{
+        {&bc_pair[0].h, &bc_pair[0].l, &de_pair[0].h, &de_pair[0].l,
+            &iy.h, &iy.l, nullptr, &af_pair[0].h},
+        {&bc_pair[1].h, &bc_pair[1].l, &de_pair[1].h, &de_pair[1].l,
+            &iy.h, &iy.l, nullptr, &af_pair[1].h}
+    },
+    rp{
+        {&bc_pair[0].w, &de_pair[0].w, &hl_pair[0].w, &sp.w},
+        {&bc_pair[1].w, &de_pair[1].w, &hl_pair[1].w, &sp.w}
+    },
+    rp2{
+        {&bc_pair[0].w, &de_pair[0].w, &hl_pair[0].w, &af_pair[0].w},
+        {&bc_pair[1].w, &de_pair[1].w, &hl_pair[1].w, &af_pair[1].w}
+    },
     reg8(&r8[0][0]),
+    regx8(&rx8[0][0]),
+    regy8(&ry8[0][0]),
     regp(&rp[0][0]),
     regp2(&rp2[0][0]),
     x(0), y(0), z(0), p(0), q(0),
@@ -39,6 +59,9 @@ void Z80RegisterSet::reset()
     ix.w = 0xFFFF;
     iy.w = 0xFFFF;
 
+    iff = 0x00;
+    im = 0x00;
+
     selectRegisterSet(0x00); // We've got to choose one, I guess?
 }
 
@@ -51,6 +74,8 @@ void Z80RegisterSet::selectRegisterSet(size_t set)
     hl = &hl_pair[registerSet];
 
     reg8 = &r8[registerSet][0];
+    regx8 = &rx8[registerSet][0];
+    regy8 = &ry8[registerSet][0];
     regp = &rp[registerSet][0];
     regp2 = &rp2[registerSet][0];
 }
--- source/src/Z80RegisterSet.h
+++ source/src/Z80RegisterSet.h
@@ -39,16 +39,22 @@ class Z80RegisterSet
         Z80Register *hl;
 
         uint8_t* r8[2][8];
+        uint8_t* rx8[2][8];
+        uint8_t* ry8[2][8];
         uint16_t* rp[2][4];
         uint16_t* rp2[2][4];
 
         uint8_t** reg8;
+        uint8_t** regx8;
+        uint8_t** regy8;
         uint16_t** regp;
         uint16_t** regp2;
 
         Z80Register operand;
         Z80Register address;
         Z80Register offset;
+        uint_fast8_t iff;
+        uint_fast8_t im;
 
         uint_fast8_t x, y, z, p, q;
         uint_fast8_t prefix;
