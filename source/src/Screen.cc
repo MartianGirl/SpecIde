cb71c7cda8bd81c6562cc1741a4c262be18c6f48
Marta Sevillano <sevillano.marta@gmail.com>
2017-05-23 22:28:38 +0200

    Some more optimization.
    - Avoid cleaning pixel buffer after each frame, by painting only on the
      updated area.

--- source/src/Screen.cc
+++ source/src/Screen.cc
@@ -4,8 +4,10 @@ Screen::Screen(size_t scale) :
     GraphicWindow(352 * scale, 312 * scale, "SpecIde"),
     scale(scale),
     xSize(352), ySize(312),
-    xPos(0), yPos(0),
-    vSync(false), hSync(false), blank(false), frame(false),
+    xPos(0), yPos(8),
+    vSync(false), vSync1d(false),
+    hSync(false), hSync1d(false),
+    blank(false), frame(false),
     texSize(xSize * ySize),
     pixels(texSize, 0xFF)
 {
@@ -30,15 +32,11 @@ void Screen::update(sf::Uint32 rgba)
     if (!vSync && vSync1d)
     {
         yPos = 0;
+
         scrTexture.update(reinterpret_cast<sf::Uint8*>(&pixels[0]));
         window.clear(sf::Color::Black);
         window.draw(scrSprite);
         window.display();
-#if SPECIDE_BYTE_ORDER == 1
-        pixels.assign(texSize, 0x000000FF);
-#else
-        pixels.assign(texSize, 0xFF000000);
-#endif
     }
     vSync1d = vSync;
 
