bbc933ae970e25ca8e6e8b723505a4427e1c26d0
Marta Sevillano <sevillano.marta@gmail.com>
2017-05-27 12:29:41 +0200

    Adapt RunCMake script to generate Release builds.

--- source/RunCMake.bat
+++ source/RunCMake.bat
@@ -3,6 +3,15 @@ set BOOST_ROOT=C:\libs\boost_1_62_0
 set Boost_NO_BOOST_CMAKE=TRUE
 
 if .%1.==.. goto USAGE
+
+if .%2.==.RELEASE. goto RELEASE
+set BUILD=Debug
+goto TOOLCHAIN
+
+:RELEASE
+set BUILD=Release
+
+:TOOLCHAIN
 if .%1.==.GNU. goto GNU
 if .%1.==.MS. goto MS
 if .%1.==.NINJAMS. goto NINJAMS
@@ -11,7 +20,7 @@ if .%1.==.NINJAGNU. goto NINJAGNU
 :USAGE
 echo Usage:
 echo.
-echo RunCMake TOOLCHAIN
+echo RunCMake TOOLCHAIN [RELEASE]
 echo.
 echo Toolchains:
 echo     GNU: MinGW (32-bit)
@@ -19,28 +28,30 @@ echo     MS:  Microsoft Visual Studio 2014 (64-bit)
 echo     NINJAGNU: MinGW compiler, Ninja build script (32-bit)
 echo     NINJAMS: Visual Studio compiler, Ninja build script (64-bit)
 echo.
+echo RELEASE: Generate Release builds, instead of Debug builds (default)
+echo.
 goto END
 
 :GNU
 set MINGW_SYSROOT=C:\MinGW
 set SFML_ROOT=C:\libs\MinGW\SFML-2.4.1
-cmake -DCMAKE_BUILD_TYPE=Debug -G "MinGW Makefiles" .
+cmake -DCMAKE_BUILD_TYPE=%BUILD% -G "MinGW Makefiles" .
 goto END
 
 :MS
 set SFML_ROOT=C:\libs\MSVC\SFML-2.4.1
-cmake -DCMAKE_BUILD_TYPE=Debug -G "Visual Studio 14 2015 Win64" .
+cmake -DCMAKE_BUILD_TYPE=%BUILD% -G "Visual Studio 14 2015 Win64" .
 goto END
 
 :NINJAMS
 set SFML_ROOT=C:\libs\MSVC\SFML-2.4.1
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=cl -G "Ninja" .
+cmake -DCMAKE_BUILD_TYPE=%BUILD% -DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=cl -G "Ninja" .
 goto END
 
 :NINJAGNU
 set MINGW_SYSROOT=C:\MinGW
 set SFML_ROOT=C:\libs\MinGW\SFML-2.4.1
-cmake -DCMAKE_BUILD_TYPE=Debug -G "Ninja" .
+cmake -DCMAKE_BUILD_TYPE=%BUILD% -G "Ninja" .
 goto END
 
 :END
