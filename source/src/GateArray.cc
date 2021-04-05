60d90d8ed132b591c90c2d2b52206c8cf7c2865e
Marta Sevillano <sevillano.marta@gmail.com>
2021-04-06 00:59:41 +0200

    Guessed Amstrad CPC frame cycle

--- source/src/GateArray.cc
+++ source/src/GateArray.cc
@@ -40,17 +40,11 @@ uint32_t GateArray::colours[32] = {
 static uint_fast32_t GateArray::pixelsX1[0x70000];
 static uint_fast32_t GateArray::pixelsX2[0x70000];
 
-uint_fast8_t GateArray::sequence[16] = {
-    0xFF, 0xFE, 0xFC, 0xF8, 0xF0, 0xE0, 0xC0, 0x80,
-    0x00, 0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F};
 
-// !(S2 | S5) => ((sequence & 0x24) != 0x00)
+void GateArray::write(uint_fast8_t byte) {
 
-void GateArray::write(uint_fast8_t byte)
-{
     // 11xx xxxx: RAM memory management (performed externally)
-    switch (byte & 0xC0)
-    {
+    switch (byte & 0xC0) {
         case 0x00:  // 00xx xxxx : Select pen
             selectPen(byte);
             break;
@@ -68,36 +62,35 @@ void GateArray::write(uint_fast8_t byte)
     }
 }
 
-uint_fast8_t GateArray::read()
-{
+uint_fast8_t GateArray::read() {
 }
 
-void GateArray::selectPen(uint_fast8_t byte)
-{
+void GateArray::selectPen(uint_fast8_t byte) {
+
     if (byte & 0x10)    // Select border
         pen = 0xFF;
     else                // Select pen
         pen = byte & 0x0F;
 }
 
-void GateArray::selectColour(uint_fast8_t byte)
-{
+void GateArray::selectColour(uint_fast8_t byte) {
+
     if (pen == 0xFF)
         border = byte & 0x1F;
     else
         pens[pen] = byte & 0x1F;
 }
 
-void GateArray::selectScreenAndRom(uint_fast8_t byte)
-{
+void GateArray::selectScreenAndRom(uint_fast8_t byte) {
+
     newMode = byte & 0x03;
     lowerRom = byte & 0x04;
     upperRom = byte & 0x08;
     interrupt = byte & 0x10;
 }
 
-void GateArray::clock()
-{
+void GateArray::clock() {
+
     counter = (counter + 1) & 0x0F;
 
     // CRTC clock, edge and level
@@ -105,7 +98,13 @@ void GateArray::clock()
     // Actually, cClk = !(sequence[counter] & 0x24) but this happens from 0x06
     // to 0x0A.
     cClkEdge = (counter == 0x0B);
-    cClk = (counter > 0x05 && counter < 0x0B);
+    cClk = cClkTable[counter];
+
+    z80_rise = z80_c & ~z80_c_d;
+
+    if (z80_rise & SIGNAL_MREQ_) {
+        gated_mreq_ = (z80_rise & SIGNAL_M1_);
+    }
 
     // 1. Sequencer works using a 8-bit shift register to generate the
     // following sequence:
@@ -120,19 +119,19 @@ void GateArray::clock()
     // 2. From here, we derive the following signals:
     //
     // PHI_ (4 MHz clock)
-    // (S1 xor S3) or (S5 xor S7), one clock later
+    // (S1 xor S3) or (S5 xor S7), delayed one clock (has digits 378C)
     // true, false, false, true, true, false, false, true
     // true, false, false, true, true, false, false, true
-    //
-    // RAS_
-    // S0 and (not S2 or S6)
-    // true, true, false, false, false, false, false, false
-    // false, false, true, true, false, false, false, false
     //
     // CASAD_
     // Same, but one clock after
     // false, true, true, false, false, false, false, false
     // false, false, false, true, true, false, false, false
+    // 
+    // CPU_
+    // not (S1 and not S7)
+    // true, true, true, true, true, true, true, true,
+    // true, true, false, false, false, false, false, false
     //
     // S3 and not S6
     // false, false, false, false, false, false, false, false
@@ -147,20 +146,20 @@ void GateArray::clock()
     // true, false, false, false, false, false, false, false
     // false, false, false, false, true, true, true, true
     //
-    // CPU_
-    // not (S1 and not S7)
-    // true, true, true, true, true, true, true, true,
-    // true, true, false, false, false, false, false, false
     //
     // CCLK_
-    // not (S2 or S5)
+    // S2 nor S5
     // false, false, false, false, false, false, true, true,
     // true, true, true, false, false, false, false, false
     //
+    // (S0 and S5 and RD_)
+    // RD_, false, false, false, false, false, false, false,
+    // false, false, false, false, false, false, RD_, RD_
+    //
     // MWE_
     // not (S0 and S5 and RD_)
-    // false, true, true, true, true, true, true, true
-    // true, true, true, true, true, true, false, false
+    // !RD_, true, true, true, true, true, true, true
+    // true, true, true, true, true, true, !RD, !RD
     //
     // 244E_
     // not (S2 and S3 and not IORQ_)
@@ -193,6 +192,10 @@ void GateArray::clock()
     // false, true, true, false, false, false, true, false,
     // false, false, false, true, true, false, false, false
     //
+    // (delayed x2)
+    // false, false, true, true, false, false, false, true,
+    // false, false, false, false, true, true, false, false
+    //
     // S2
     // true, true, true, false, false, false, false, false
     // false, false, false, true, true, true, true, true
@@ -201,15 +204,49 @@ void GateArray::clock()
     // true, true, true, true, true, true, true, true,
     // true, true, true, true, true, false, true, true
     //
-    // (delayed x2)
-    // false, false, true, true, false, false, false, true
-    // false, false, false, false, true, true, false, false
+    // (not S4 or S5) or #MREQ (gated the refresh MREQ)
+    // true, true, true, true, true, true, true, true
+    // true, true, true, true, true, #MREQ, true, true
+    //
+    // signal above and S2
+    // true, true, true, false, false, false, false, false
+    // false, false, false, true, true, #MREQ, true, true
+    //
+    // ANG gate U712
+    // #MREQ, true, true, false, false, false, false, false,
+    // false, false, false, true, true, #MREQ, #MREQ, #MREQ,
     //
     // (delayed x1 or delayed x2)
     // false, true, true, true, false, false, true, true
     // false, false, false, true, true, true, false, false
     //
-
+    // #CAS (delayed x1 or delayed x2 or U712)
+    // #MREQ, true, true, true, false, false, true, true,
+    // false, false, false, true, true, true, #MREQ, #MREQ
+    //
+    // RAS_
+    // S0 and (not S2 or S6)
+    // true, true, false, false, false, false, false, false
+    // false, false, true, true, false, false, false, false
+    //
+    // 74LS153 multiplexers (0 = A15-A9,A0, 1 = A8-A1,
+    // 2 = M13-M12, RA2-0, M9-M8-CCLK, 3 = M7-M0)
+    // 2, 3, 3, 2, 2, 2, 2, 2,
+    // 2, 2, 0, 1, 1, 0, 0, 0
+    //
+    // This means that the frame (starting at 0) is:
+    // 0: nothing
+    // 1: nothing - wait
+    // 2: video row address latched - wait
+    // 3: nothing - wait
+    // 4: video col address latched - wait
+    // 5-6-7: get data - wait
+    // 8: video col address latched - wait
+    // 9-a-b: get data - wait
+    // c: cpu row address latched - go
+    // d: nothng
+    // e: cpu col address latched
+    // f-0: get data
 }
 
 // vim: et:sw=4:ts=4
--- source/src/GateArray.h
+++ source/src/GateArray.h
@@ -35,6 +35,7 @@ class GateArray
 
         bool cClk = false;
         bool cClkEdge = false;
+        bool gatedMREQ_ = false;
 
         void selectPen(uint_fast8_t byte);
         void selectColour(uint_fast8_t byte);
@@ -44,7 +45,49 @@ class GateArray
         static uint_fast32_t pixelsX1[0x70000];
         static uint_fast32_t pixelsX2[0x70000];
 
-        static uint_fast8_t sequence[16];
+        /**
+         * Gate array sequence.
+         *
+         * With the information available, looks like this sequence is never reset.
+         * In order to be reset, RST needs to be hign, and M1, IORQ and RD need to be
+         * low. This last combination never happens in a Z80 CPU.
+         */
+        static uint_fast8_t constexpr sequence[16] = {
+            0xFF, 0xFE, 0xFC, 0xF8, 0xF0, 0xE0, 0xC0, 0x80,
+            0x00, 0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F
+        };
+
+        /**
+         * 4MHz CPU clock.
+         *
+         * The equation is (S1 ^ S3) | (S5 ^ S7), so it's true for any value of the
+         * sequencer with digits 3, 7, 8 or C.
+         * The result is latched, so this table is shifted right one position.
+         */
+        static bool constexpr phiTable[16] = {
+            true, false, false, true, true, false, false, true,
+            true, false, false, true, true, false, false, true
+        };
+
+        /**
+         * 1MHz clock.
+         *
+         * The equation is !(S2 | S5), so it's true from 0xC0 to 0x03.
+         */
+        static bool constexpr cClkTable[16] = {
+            false, false, false, false, false, false, true, true,
+            true, true, true, false, false, false, false, false
+        };
+
+        /**
+         * Gate Array READY signal.
+         *
+         * This is the Z80 #WAIT signal.
+         */
+        static bool constexpr readyTable[16] = {
+            true, false, false, false, false, false, false, false,
+            false, false, false, false, true, true, true, true
+        };
 };
 
 // vim: et:sw=4:ts=4
